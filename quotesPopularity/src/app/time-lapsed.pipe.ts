d6fa17dc4d30e19db669949b878c5c9e79141bac
wdmuthiora <wdmuthiora@gmail.com>
2021-10-11 22:04:46 +0300

    Configured TimeLapsed pipe's logic.

--- quotesPopularity/src/app/time-lapsed.pipe.ts
+++ quotesPopularity/src/app/time-lapsed.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeLapsedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today: Date = new Date();
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDay()
+    );
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInADay = 86400;
+    var dateDifferenceInSeconds = dateDifference * 0.001;
+    var timeLapsed = dateDifferenceInSeconds / secondsInADay;
+    if (timeLapsed >= 1 && value > todayWithNoTime) {
+      return timeLapsed;
+    } else {
+      return 0;
+    }
   }
 
 }
