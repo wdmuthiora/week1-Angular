e189dc3094b33a67f4b4ca9bba0d5bd699f56bd6
wdmuthiora <wdmuthiora@gmail.com>
2021-10-11 15:10:24 +0300

    Created delete logic inside the Quotes component.

--- quotesPopularity/src/app/quotes/quotes.component.ts
+++ quotesPopularity/src/app/quotes/quotes.component.ts
@@ -13,13 +13,25 @@ export class QuotesComponent implements OnInit {
     new Quote('Dennis', 'Quote number three'),
   ];
   //increment the upvote
-  upVote(index:number) {
+  upVote(index: number) {
     this.quotes[index].upVote += 1;
-  };
+  }
 
   //increment the downvote
-  downVote(index:number) {
+  downVote(index: number) {
     this.quotes[index].downVote += 1;
+  }
+
+  //delete a quote
+  delete(index: number) {
+
+    let toPromptUser = confirm(
+      `Are you sure you want to delete this quote ${this.quotes[index].quoteText}?`
+    );
+    if (toPromptUser) {
+      this.quotes.splice(index, 1);
+    }
+
   };
 
   constructor() {}
