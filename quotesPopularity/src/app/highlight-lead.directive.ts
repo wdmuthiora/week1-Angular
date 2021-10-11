83afb7983b036b29e074868860d493c1f17d6fd0
wdmuthiora <wdmuthiora@gmail.com>
2021-10-11 20:47:40 +0300

    Built the Highlight-Lead directive's logic.

--- quotesPopularity/src/app/highlight-lead.directive.ts
+++ quotesPopularity/src/app/highlight-lead.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightLead]'
 })
 export class HighlightLeadDirective {
-
-  constructor() { }
+  
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.backgroundColor='rgba(0, 128, 0, 0.3)';
+   }
 
 }
