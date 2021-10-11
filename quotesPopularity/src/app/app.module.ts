cfce02073bdf0e62a77b7dd41ed0070bdbfda111
wdmuthiora <wdmuthiora@gmail.com>
2021-10-11 12:08:41 +0300

    Created quotes' component

---
+++ quotesPopularity/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
