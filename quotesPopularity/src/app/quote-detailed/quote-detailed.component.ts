49ed4cfb70152884fc71f044f1900dadc7bc5524
wdmuthiora <wdmuthiora@gmail.com>
2021-10-11 13:57:52 +0300

    Iterated through the quotes' array, to display them on each card. Did input-binding to quote (from quotes to quote-detailed component)

--- quotesPopularity/src/app/quote-detailed/quote-detailed.component.ts
+++ quotesPopularity/src/app/quote-detailed/quote-detailed.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-quote-detailed',
   templateUrl: './quote-detailed.component.html',
-  styleUrls: ['./quote-detailed.component.css']
+  styleUrls: ['./quote-detailed.component.css'],
 })
 export class QuoteDetailedComponent implements OnInit {
+  @Input() quote!: Quote;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
